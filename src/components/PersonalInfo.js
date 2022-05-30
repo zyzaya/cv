b6fa08f7daa4425c86c7795c0f3b7d8f73b9e7a1
Riley Sherlock <riley.sherlock@mts.net>
2022-05-30 14:40:27 -0500

    create PersonalInfo component

---
+++ src/components/PersonalInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class PersonalInfo extends React.Component {
+  render() {
+    return (
+      <div className="div">
+        <div className="">Name</div>
+        <div className="Email">Email</div>
+        <div className="Phone">Phone Number</div>
+      </div>
+    );
+  }
+}
