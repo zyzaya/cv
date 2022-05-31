e26c098beb59e279c805641d96a1fe1b9e08a4af
Riley Sherlock <riley.sherlock@mts.net>
2022-05-31 16:08:59 -0500

    implement values changing in ExperienceSection

--- src/components/ExperienceInfo.js
+++ src/components/ExperienceInfo.js
@@ -11,30 +11,6 @@ export default class ExperienceInfo extends React.Component {
     return (
       <div>
         <h1>Experience</h1>
-        <Text
-          name="company"
-          type="text"
-          edit={this.state.edit}
-          onChange={(val) => console.log(val)}
-        />
-        <Text
-          name="position"
-          type="text"
-          edit={this.state.edit}
-          onChange={(val) => console.log(val)}
-        />
-        <Text
-          name="start"
-          type="date"
-          edit={this.state.edit}
-          onChange={(val) => console.log(val)}
-        />
-        <Text
-          name="finish"
-          type="date"
-          edit={this.state.edit}
-          onChange={(val) => console.log(val)}
-        />
         <button onClick={this.addSection}>Add Section</button>
         <button onClick={this.editInfo}>
           {this.state.edit ? 'Finish' : 'Edit'}
--- src/components/ExperienceSection.js
+++ src/components/ExperienceSection.js
@@ -4,6 +4,43 @@ import Text from './Text';
 export default class ExperienceSection extends React.Component {
   constructor(props) {
     super(props);
+
+    this.changeCompany = this.changeCompany.bind(this);
+    this.changePosition = this.changePosition.bind(this);
+    this.changeStart = this.changeStart.bind(this);
+    this.changeFinish = this.changeFinish.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  changeCompany(value) {
+    this.setState({ company: value });
+    this.handleChange({ company: value });
+  }
+
+  changePosition(value) {
+    this.setState({ position: value });
+    this.handleChange({ position: value });
+  }
+
+  changeStart(value) {
+    this.setState({ start: value });
+    this.handleChange({ start: value });
+  }
+
+  changeFinish(value) {
+    this.setState({ finish: value });
+    this.handleChange({ finish: value });
+  }
+
+  handleChange(value) {
+    let info = {
+      company: value.company || this.state.company,
+      position: value.position || this.state.position,
+      start: value.start || this.state.start,
+      finish: value.finish || this.state.finish,
+    };
+    this.props.onChange(info);
   }
 
   handleDelete() {
@@ -22,25 +59,25 @@ export default class ExperienceSection extends React.Component {
           name="company"
           type="text"
           edit={this.state.edit}
-          onChange={(val) => console.log(val)}
+          onChange={this.changeCompany}
         />
         <Text
           name="position"
           type="text"
           edit={this.state.edit}
-          onChange={(val) => console.log(val)}
+          onChange={this.changePosition}
         />
         <Text
           name="start"
           type="date"
           edit={this.state.edit}
-          onChange={(val) => console.log(val)}
+          onChange={this.changeStart}
         />
         <Text
           name="finish"
           type="date"
           edit={this.state.edit}
-          onChange={(val) => console.log(val)}
+          onChange={this.changeFinish}
         />
         {remove}
       </div>
