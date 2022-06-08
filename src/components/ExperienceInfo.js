2cc424b2d6f9a6953857097ab6f9c90d4d0787ca
Riley Sherlock <riley.sherlock@mts.net>
2022-06-08 16:34:10 -0500

    convert ExperienceInfo to function component

--- src/components/ExperienceInfo.js
+++ src/components/ExperienceInfo.js
@@ -1,78 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import uniqid from 'uniqid';
 import ExperienceSection from './ExperienceSection';
 import '../styles/ExperienceInfo.css';
 
-export default class ExperienceInfo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { edit: props.edit, sections: [] };
-    this.addSection = this.addSection.bind(this);
-    this.editInfo = this.editInfo.bind(this);
-    this.deleteSection = this.deleteSection.bind(this);
-  }
+export default function ExperienceInfo(props) {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { edit: props.edit, sections: [] };
+  //   this.addSection = this.addSection.bind(this);
+  //   this.editInfo = this.editInfo.bind(this);
+  //   this.deleteSection = this.deleteSection.bind(this);
+  // }
 
-  editInfo() {
-    if (this.state.edit) {
+  const [edit, setEdit] = useState(props.edit);
+  const [sections, setSections] = useState([]);
+
+  function editInfo() {
+    if (edit) {
       let info = [];
-      this.state.sections.map((val) =>
-        info.push({
-          company: val.company,
-          position: val.position,
-          start: val.start,
-          finish: val.finish,
-        })
-      );
-      this.props.onEdit(info);
+      sections.map((val) => info.push({ ...val }));
+      props.onEdit(info);
     }
-    this.setState({ edit: !this.state.edit });
+    setEdit(!edit);
   }
 
-  addSection() {
-    this.setState((state, props) => ({
-      sections: state.sections.concat({
+  function addSection() {
+    setSections(
+      sections.concat({
         key: uniqid(),
         company: '',
         position: '',
         start: '',
         finish: '',
-      }),
-    }));
-  }
-
-  editSection(key, info) {
-    this.setState((state, props) => ({
-      sections: state.sections.map((e) =>
-        e.key === key ? { key: e.key, ...info } : e
-      ),
-    }));
-  }
-
-  deleteSection(key) {
-    this.setState((state, props) => ({
-      sections: state.sections.filter((e) => e.key !== key),
-    }));
-  }
-
-  render() {
-    return (
-      <div className="ExperienceInfo">
-        <h1>Experience</h1>
-        <ul>
-          {this.state.sections.map((e) => (
-            <ExperienceSection
-              key={e.key}
-              edit={this.state.edit}
-              onChange={(info) => this.editSection(e.key, info)}
-              onDelete={() => this.deleteSection(e.key)}
-            />
-          ))}
-        </ul>
-        <button onClick={this.addSection}>Add Section</button>
-        <button onClick={this.editInfo}>
-          {this.state.edit ? 'Finish' : 'Edit'}
-        </button>
-      </div>
+      })
     );
   }
+
+  function editSection(key, info) {
+    setSections(
+      sections.map((e) => (e.key === key ? { key: e.key, ...info } : e))
+    );
+  }
+
+  function deleteSection(key) {
+    setSections(sections.filter((e) => e.key !== key));
+  }
+
+  return (
+    <div className="ExperienceInfo">
+      <h1>Experience</h1>
+      <ul>
+        {sections.map((e) => (
+          <ExperienceSection
+            key={e.key}
+            edit={edit}
+            onChange={(info) => editSection(e.key, info)}
+            onDelete={() => deleteSection(e.key)}
+          />
+        ))}
+      </ul>
+      <button onClick={addSection}>Add Section</button>
+      <button onClick={editInfo}>{edit ? 'Finish' : 'Edit'}</button>
+    </div>
+  );
 }
