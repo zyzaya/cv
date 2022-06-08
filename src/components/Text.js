c813bb3dba9e21487a7c227722ee9c54ec3235b3
Riley Sherlock <riley.sherlock@mts.net>
2022-06-08 16:20:57 -0500

    covnert Text into function component

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,36 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Text.css';
 
-export default class Text extends React.Component {
-  constructor(props) {
-    super(props);
-    this.editValue = this.editValue.bind(this);
-    this.state = { value: '' };
+export default function Text(props) {
+  const [value, setValue] = useState('');
+
+  function editValue(e) {
+    setValue(e.target.value);
+    props.onChange(e.target.value);
   }
 
-  editValue(e) {
-    this.setState({ value: e.target.value });
-    this.props.onChange(e.target.value);
-  }
-
-  render() {
-    if (this.props.edit)
-      return (
-        <div className="Text">
-          <label htmlFor="name">{this.props.name}</label>
-          <input
-            type={this.props.type}
-            onChange={this.editValue}
-            value={this.state.value}
-          />
-        </div>
-      );
-    else
-      return (
-        <div className="Text">
-          <label htmlFor="name">{this.props.name}</label>
-          <span>{this.state.value}</span>
-        </div>
-      );
+  if (props.edit) {
+    return (
+      <div className="Text">
+        <label htmlFor="name">{props.name}</label>
+        <input type={props.type} onChange={editValue} value={value} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="Text">
+        <label htmlFor="name">{props.name}</label>
+        <span>{value}</span>
+      </div>
+    );
   }
 }
